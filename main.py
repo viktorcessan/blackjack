72efafb71e509046f898bbca170585caafc8245d
viktorcessan <viktor@cessan.se>
2023-11-25 23:09:51 +0100

    First commit. Only contains an input and while loop.

---
+++ main.py
@@ -0,0 +1,10 @@
+a = input("Vill du ha ett kort? ")
+a = int(a)
+b = 2
+
+while (a > b):
+    a = input("Vill du ha ett kort? ")
+    print(int(a))
+#while loop that says the program keeps running as long as a is larger than b
+
+#todo add a random generator
