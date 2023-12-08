09a1153be0383e41d25593e06dc4eca04142fdcc
viktorcessan <viktor@cessan.se>
2023-12-08 14:58:09 +0100

    Simplified for-loop. Instead of repeating it four times, the for loop runs four times. Also created random shuffle function.

--- Simple Black Jack.py
+++ Simple Black Jack.py
@@ -1,17 +1,17 @@
 print("Welcome to Programmatic Black Jack.")
 
 deck = []
-for number in range(1,11):
-    deck.append(number)
-for number in range(1,11):
-    deck.append(number)
-for number in range(1,11):
-    deck.append(number)
-for number in range(1,11):
-    deck.append(number)
+for number in range(1,5):
+    for number in range(1, 11):
+        deck.append(number)
 
 for number in range(1,13):
     deck.append(10)
+
+print(deck)
+
+import random
+random.shuffle(deck)
 
 print(deck)
 print(len(deck))
