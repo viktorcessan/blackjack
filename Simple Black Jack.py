848bd53a83c28f8ccb7e9d6cd13a4f4dc597faec
viktorcessan <viktor@cessan.se>
2023-12-08 14:40:15 +0100

    Created a dirty black jack version with hard coded deck, and first comitt with a programmatic deck

--- Simple Black Jack.py
+++ Simple Black Jack.py
@@ -1,3 +1,17 @@
-#Skapa en dirty black jack deck. Printa decket.
-#Skapa den sedan med for loop. popa bort när en använts.
-#Lägg på valörer. Avancerad for loop.
+print("Welcome to Programmatic Black Jack.")
+
+deck = []
+for number in range(1,11):
+    deck.append(number)
+for number in range(1,11):
+    deck.append(number)
+for number in range(1,11):
+    deck.append(number)
+for number in range(1,11):
+    deck.append(number)
+
+for number in range(1,13):
+    deck.append(10)
+
+print(deck)
+print(len(deck))
